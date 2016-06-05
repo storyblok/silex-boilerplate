4053347fc6fc4e43561266e3de3ded306e2f58a2
Alexander Feiglstorfer <delooks@gmail.com>
2016-06-05 10:21:41 +0200

    adds functionality to unpublish story

--- webapp/app.php
+++ webapp/app.php
@@ -13,6 +13,17 @@ $app->get('/welcome', function() use($app) {
     return $app['twig']->render('components/welcome.twig');
 });
 
+$app->get('/clear_cache', function() use($app) {
+	$app['storyblok']->deleteCacheBySlug($app['request']->get('slug'));
+
+	if ($app['request']->get('action') == 'published') {
+		// Fill the cache immediatly after publishing the story
+		$app['storyblok']->getStoryBySlug($app['request']->get('slug'));
+	}
+
+    return $app->json(array('success' => true));
+});
+
 $app->mount('/{_locale}', new App\Controllers\Web());
 
 $app->run();
