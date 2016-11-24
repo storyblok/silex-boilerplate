bb2631793911c42ac7d9a2ae6e72a953f06354da
Dominik Angerer <dominikangerer1@gmail.com>
2016-11-24 16:09:02 +0100

    changed default config

--- webapp/config.php
+++ webapp/config.php
@@ -4,5 +4,5 @@ $app['debug']                   = true;
 $app['config.twig.cache']       = false;
 
 $app['config.show_help']  = true;
-$app['config.home']  = '###insert-home-slug###';
-$app['storyblok.privateToken']  = '###insert-private-token###';
+$app['config.home']  = 'insert-home-slug';
+$app['storyblok.privateToken']  = 'insert-private-token';
