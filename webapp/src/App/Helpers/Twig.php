1bf3ff4c40e84561c478530c9ac56bac26a93d20
DominikAngerer <me@dominikangerer.com>
2017-01-20 12:37:55 +0100

    Introduced getStories

--- webapp/src/App/Helpers/Twig.php
+++ webapp/src/App/Helpers/Twig.php
@@ -19,6 +19,31 @@ class Twig implements ServiceProviderInterface
         $app['twig']->addFunction(new \Twig_SimpleFunction('asset', function ($asset) use ($app) {
             return '/' . $asset;
         }));
+        
+        $app['twig']->addFunction(new \Twig_SimpleFunction('getStories', function ($starts_with, $sort_by = null, $with_tag = null, $page = 0, $per_page = 25) use ($app) {
+            try {
+                $starts_with = $app['config.locale'] . '/' . $starts_with;
+
+                $app['storyblok']->getStories(
+                    array_filter(
+                        array(
+                        'starts_with' => $starts_with,
+                        'with_tag' => $with_tag,
+                        'sort_by' => $sort_by,
+                        'per_page' => $per_page,
+                        'page' => $page
+                        )
+                    )
+                );
+
+                $data = $app['storyblok']->getStoryContent();
+
+                return $data['stories'];
+            } catch (\Exception $e) {
+                throw new Exception($e);
+            }
+            return null;
+        }));
     }
 
     public function boot(Application $app)
